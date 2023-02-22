84dbf828b9fe0d897454541279b81e6d7f4a6015
Jefferson Pababero <jefferson.pababero.com>
2023-02-22 15:43:16 +0800

    ruby-variables

---
+++ ruby-variables/declaring_a_variable.rb
@@ -0,0 +1,28 @@
+age = 18
+
+age = 18 + 5
+
+age = 18
+
+age
+
+age = 33
+
+age
+
+age = 18
+
+age = age + 4
+
+age = 18
+
+age += 4
+
+age = 18
+age -= 2
+
+cash = 10
+cash *= 2
+
+temperature = 40
+temperature /= 10
---
+++ ruby-variables/variables_are_references.rb
@@ -0,0 +1,11 @@
+desired_location = "Barcelona"
+johns_location = desired_location
+
+desired_location
+johns_location
+
+
+johns_location.upcase!
+
+desired_location
+johns_location
